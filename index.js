ef13560fb382e995520b0f99a00ca53b9d7f1f39
Min1216-g <gwonmin403@gmail.com>
2026-05-03 12:24:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello render");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server running");
+});
